ecf55791062580bb0afc287016db0e068d3842f6
Philippe Ombredanne <pombredanne@nexb.com>
2020-03-06 18:22:43 -0800

    Rename last_modified to last_modified_date
    Also streamline the model and add convenience wrappers.
    Create initial migration
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/clearcode/models.py
+++ src/clearcode/models.py
@@ -17,7 +17,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import gzip
+import json
+
 from django.db import models
+
+
+class VirtualFileStore:
+    """
+    Convenience wrapper to access CDitems as if they would be concrete files.
+    """
+    @classmethod
+    def walk(self, prefix=None, since=None):
+        """
+        Yield tuples of (path, data, last modified) for CD items.
+        Optionally return items that have a certain path prefix.
+        Optionally return items that have have been modified `since` a datetime.
+        """
+        items = CDitem.objects.all()
+        if prefix:
+            items = items.filter(path__startswith=prefix)
+        if since:
+            items = items.filter(last_modified_date__ge=since)
+        for item in items:
+            yield item.path, item.data_content(), item.last_modified_date
 
 
 class CDitem(models.Model):
@@ -34,7 +57,13 @@ class CDitem(models.Model):
         help_text='Actual gzipped JSON content.'
     )
 
-    last_modified = models.DateTimeField(
-        help_text='Date that this record was last modified.',
-        db_index=True,
+    last_modified_date = models.DateTimeField(
+        help_text='Date and time that this record was last modified.',
+        auto_now=True,  # Automatically set to now on object save()
     )
+
+    def data_content(self):
+        """
+        Return the data content deserialized from the content field.
+        """
+        return json.loads(gzip.decompress(self.content))
